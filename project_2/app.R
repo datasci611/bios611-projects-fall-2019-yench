c2fdb19ab5703ac35ab1f10c90950a4410e0519b
yench <yench@users.noreply.github.com>
2019-10-20 16:27:03 -0400

    Project 2 update

---
+++ project_2/app.R
@@ -0,0 +1,121 @@
+library(shiny)
+library(tidyverse)
+
+source("helper_functions.R")
+
+# Call the clean_helper function to read in and clean the data as required
+data = clean_helper('data/UMD_Services_Provided_20190719.tsv.txt')
+
+# Call the B_data_helper function to prepare data for plot B
+data_b = B_data_helper(data)
+
+# Construct the ui
+ui = fluidPage(
+  
+  titlePanel('BIOS611 Project 2   Fall, 19   Yen Chang'),
+  
+  # Define the sidebar panel 
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      # Plot A1: a histogram of counts of a selected service in a selected period.
+      # Plot A2: a pie chart showing the percentage of visits seeking the selected service in the selected period.
+      h4(helpText('A. Counts of a particular service')),
+      helpText('This widget creates a histogram and a pie chart of counts of a particular type of service in a selected period. Please select the type of service and the time period of interest.'),
+      
+      
+      # Input A_service: types of service to select from
+      radioButtons(inputId = 'A_service',
+                     label = 'Type of services',
+                   choices = c('Food', 'Clothing', 'Diapers', 'School Kits', 'Hygiene Kits', 'Bus Tickets')),
+      
+      # Input A_year: years to display
+      sliderInput(inputId = 'A_year',
+                  label = 'Years',
+                  min = 1999,
+                  max = 2019,
+                  value = c(1999,2019),
+                  sep = ''),
+      
+      br(),
+      
+      # Plot B: Relationship between family size and use of services
+      h4(helpText('B. Family size and services')),
+      
+      helpText('This widget creates a scatter plot looking at the relationship between family size and selected summary statistics for a selected type of service.'),
+      
+      radioButtons(inputId = 'B_service',
+                   label = 'Type of service',
+                   choices = c('Food Provided for', 'Food in Pounds', 'Clothing', 
+                               'Diapers', 'School Kits', 'Hygiene Kits', 'Bus Tickets')),
+      
+      radioButtons(inputId = 'B_y_axis',
+                   label = 'Summary to display on the y-axis',
+                   choices = c('Count of visits attributable to the selected service',
+                               'Proportion of visits attributable to the selected service',
+                               'Average number of portions provided in each visit for the selected service'))
+    ),
+    
+    
+    # Main panel to display outputs
+    mainPanel(
+      
+      # Output: A1 and A2 and related text 
+      plotOutput(outputId = 'A1plot'),
+      
+      br(),
+      
+      textOutput(outputId = 'Acount'),
+      
+      br(),
+      
+      plotOutput(outputId = 'A2plot'),
+
+      br(),
+      
+      # Output: B
+      plotOutput(outputId = 'Bplot'),
+      
+      br(), br(),
+      
+      # Output: Data source
+      textOutput(outputId = 'datasource')
+    )
+  )
+)
+
+
+# Define server
+server = function(input, output) {
+  
+  # Creates plots and text output using helper functions and links them to ui
+  
+  # A_data_helper function creates a list containing the a subset of data for creating 
+  # plots A1 and A2, as well as counts for both plots and text output. The other helper
+  # functions take the list produced by A_data_helper to create plots or text. 
+  output$A1plot = renderPlot({
+    service_data_list = A_data_helper(data, input$A_service, input$A_year)
+    A1_plot_helper(service_data_list, input$A_service, input$A_year)
+  })
+  
+  output$A2plot = renderPlot({
+    service_data_list = A_data_helper(data, input$A_service, input$A_year)
+    A2_plot_helper(service_data_list, input$A_service)
+  })
+  
+  output$Acount = renderText({
+    service_data_list = A_data_helper(data, input$A_service, input$A_year)
+    paste('Total number of services in this period:', service_data_list$total_count, '.', sep = '')
+  })
+  
+  output$Bplot = renderPlot({
+    B_plot_helper(data_b, input$B_y_axis, input$B_service)
+  })
+  
+  output$datasource = renderText({
+    'Data source: Urban Ministries of Durham'
+  })
+}
+
+shinyApp(ui = ui, server = server)
---
+++ project_2/helper_functions.R
@@ -0,0 +1,183 @@
+# This helper function reads in and prepares the UMD service data for analysis
+clean_helper = function(data_path){
+  
+  # Read in dataset
+  umd_df = read_tsv(data_path)
+  
+  # Clean data
+  cleaned = umd_df %>%
+    
+    select(-'Client File Merge',-(Field1:Field3)) %>%          # Remove variables not used in the app
+    
+    rename(      'id' = 'Client File Number',                  # Rename variables
+                'bus' = 'Bus Tickets (Number of)',
+               'note' = 'Notes of Service',
+             'food.n' = 'Food Provided for',
+             'food.p' = 'Food Pounds',
+            'clothes' = 'Clothing Items',
+            'diapers' = 'Diapers',
+             'school' = 'School Kits',
+            'hygiene' = 'Hygiene Kits') %>%  
+    
+    mutate(date  = as.Date(Date, "%m/%d/%Y"),                   # Transform dates
+           year  = as.numeric(format(date, '%Y')),
+           month = as.numeric(format(date, "%m"))) %>%
+    
+    filter(between(year, 1999, 2019)) %>%                       # Keep only observations occuring between 1999 and 2019 
+  
+    mutate(food.ind = food.n > 0 | food.p > 0)                  # Create an indicator variable with 1 specifying seeking for food service
+  return(cleaned)
+}
+
+
+# This helper function prepares data to be used in graphing A1 and A2 plots
+A_data_helper = function(data,service,yr){
+  service_var = switch(service,'Food' = 'food.ind', 'Clothing'='clothes', 'Diapers' = 'diapers',
+                               'School Kits' = 'school', 'Hygiene Kits' = 'hygiene', 'Bus Tickets' = 'bus')
+  
+  # Create a text string with the years selected, to be used in the histogram title
+  text = paste('(',min(yr),' to ',max(yr),')',sep='') 
+  
+  
+  total_data = data %>%                       # Keep only the visits in the selected period
+    filter(between(year,yr[1],yr[2]))
+  
+  total_count = dim(total_data)[1]            # Count the number of all services provided in the selected period
+  
+  service_data = total_data %>%               # Keep only visits seeking for the selected service
+    filter(get(service_var) > 0) 
+  
+  service_count = dim(service_data)[1]        # Count the number of the selected service provided in the selected period
+  
+  return(list(text = text,                    # Return objects created in this function as a list
+              total_count = total_count,
+              service_data = service_data,
+              service_count = service_count))
+}
+
+
+# This helper function creates plot A1
+A1_plot_helper = function(data,service,yr){
+  
+  ggplot(data = data$service_data)+
+    
+    scale_x_continuous(breaks = seq(yr[1],yr[2],1))+
+    
+    labs(title = paste('A1. Counts of Visits Seeking', service, data$text),
+         x = 'Year',
+         y = 'Count of visits')+ 
+    geom_bar(aes(year), fill = "#78517b")+
+    
+    theme( plot.title = element_text(size = 22),
+          axis.text.x = element_text(angle = 45, hjust = 1))
+  
+}
+
+
+# This helper function creates plot A2
+A2_plot_helper = function(data,service){
+  
+  count.data = data.frame(
+    type = c(service, 'Others'),
+    n = c(data$service_count, data$total_count - data$service_count),    # The count attributable and the count not attributable to a selected service
+    percent = 100 * c(data$service_count/data$total_count, 1-data$service_count/data$total_count)   # The proportion of above
+  )
+
+  count.data$type = factor(count.data$type, levels = c(service,'Others'))    # Setting factor level for the right order of variable display in the pie chart
+  
+  ggplot(data = count.data,aes("", percent, fill = type))+
+    
+    geom_bar(stat = "identity")+
+    
+    coord_polar("y", start = 0, direction = -1)+           # Pie Chart
+    
+    labs(title = paste('A2. Percentage of Visits Seeking', service),
+         fill = '',
+         caption = 'Some slices might not show on the pie chart due to extremely small percentages.')+
+    
+    scale_fill_manual(values = c("#ee6233","#3AA496"))+
+    
+    geom_text(aes(label = paste(round(percent, 2), '%')),position = position_stack(vjust = 0.5))+
+    
+    theme(   plot.title = element_text(size = 22),
+           plot.caption = element_text(size = 12, hjust= 0.5),
+           axis.title.x = element_blank(),
+           axis.title.y = element_blank(),
+             axis.ticks = element_blank(),
+           panel.border = element_blank(),
+             panel.grid = element_blank())
+}
+
+
+# This helper function perpares data for plot B
+B_data_helper = function(data){
+  household = data %>%
+    
+    arrange(id,date) %>%
+    group_by(id) %>%
+    
+    mutate(food.n.trunc = ifelse(food.n > 50, NA, food.n),            # Food Provided for > 50 not reasonable, set to missing
+           food.p.trunc = ifelse(food.p > 100, NA, food.p),           # Food in Pounds > 100 not reasonable, set to missing
+                diapers = ifelse(diapers > 2000, NA, diapers)) %>%    # Diapers > 2000 not reasonable, set to missing
+                
+    summarize(size = max(food.n.trunc, 1, na.rm = TRUE),              # Estimate the size of a household by the maximum value of Food Provided for
+              count_visit = n(),                                      # Count numbers of visits, total and by service type
+              count_food.n = sum(food.n > 0 | food.p > 0, na.rm = TRUE),
+              count_food.p = count_food.n,
+              count_clothes = sum(clothes > 0, na.rm = TRUE),
+              count_diapers = sum(diapers > 0, na.rm = TRUE),
+              count_school = sum(school > 0, na.rm = TRUE),
+              count_hygiene = sum(hygiene > 0, na.rm = TRUE),
+              count_bus = sum(bus > 0, na.rm = TRUE),
+              
+              prop_food.n = count_food.n/count_visit,           # Count proportion of visits attributable to a specific type of service
+              prop_food.p = prop_food.n,
+              prop_clothes = count_clothes/count_visit,
+              prop_diapers = count_diapers/count_visit,
+              prop_school = count_school/count_visit,
+              prop_hygiene = count_hygiene/count_visit,
+              prop_bus = count_bus/count_visit,
+              
+              sum_food.n = sum(food.n.trunc, na.rm = TRUE),     # The total number of portions of a service provided to each household
+              sum_food.p = sum(food.p.trunc, na.rm = TRUE),
+              sum_clothes = sum(clothes, na.rm = TRUE),
+              sum_diapers = sum(diapers, na.rm = TRUE),
+              sum_school = sum(school, na.rm = TRUE),
+              sum_hygiene = sum(hygiene, na.rm = TRUE),
+              sum_bus = sum(bus, na.rm = TRUE),
+              
+              average_food.n = sum_food.n/count_food.n,         # The average number of portions of a service provided to each household when it came for that service
+              average_food.p = sum_food.p/count_food.p,
+              average_clothes = sum_clothes/count_clothes,
+              average_diapers = sum_diapers/count_diapers,
+              average_school = sum_school/count_school,
+              average_hygiene = sum_hygiene/count_hygiene,
+              average_bus = sum_bus/count_bus) 
+  }
+
+B_plot_helper = function(data, y_axis, service){
+  
+  y = switch(y_axis, 'Count of visits attributable to the selected service' = 'count',
+                     'Proportion of visits attributable to the selected service' = 'prop',
+                     'Average number of portions provided in each visit for the selected service' = 'average')
+  
+  service_var = switch(service,'Food Provided for' = 'food.n', 'Food in Pounds' = 'food.p', 'Clothing'='clothes', 
+                               'Diapers' = 'diapers', 'School Kits' = 'school', 'Hygiene Kits' = 'hygiene', 'Bus Tickets' = 'bus')
+  
+  summary_var = paste(y, service_var, sep = '_')
+  
+  ggplot(data = data)+
+    geom_count(aes(x = size, y = get(summary_var), color = ..n..))+
+    
+    labs(title = paste('B. Joint Distribution of Family Size and', service),
+         x = 'Household size',
+         y = sub('the selected service', service, y_axis),
+         caption = '1. Household size estimated by the maximum value of Food Provided for for that household. 
+         \n2. Color and size of dots correspond to frequency of a value. 
+         \n3. Food Provided for and Food in Pounds have the same count and proportion because both were for food.')+
+   
+    theme(plot.title = element_text(size = 22),
+          plot.caption = element_text(size = 12, hjust = 0))+
+    
+    guides(color = 'legend')
+}
